afbed8ca2e3352482c4bbc5c953d96591297152e
unknown <felipedv07@gmail.com>
2018-04-01 23:11:26 -0300

    ajustar estrutura do modal

--- js/mostrapokemon.js
+++ js/mostrapokemon.js
@@ -6,6 +6,16 @@ function mostraPokemon(url) {
   //     OS COMPONENTES HTML NO MODAL
   //  4. ABRIR MODAL (.modal) COM JAVASCRIPT
   //     (http://getbootstrap.com/docs/4.0/components/modal/#via-javascript)
+  const URL = url;
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', URL, true);
+  xhr.send();
+  xhr.onload = function(e) {
+    const res = JSON.parse(xhr.response);
+    console.log(res);
+    populaModal(res);
+    $('#modal').modal('show');
+  }
 }
 
 
@@ -17,4 +27,49 @@ function populaModal(pokemon) {
   //     (http://getbootstrap.com/docs/4.0/components/modal/#modal-components)
   //  2. LINKAR TODOS OS COMPONENTES COM O MODAL .modal
   //  3. SEMPRE QUE FECHAR O MODAL LIMPAR O CONTEUDO ADICIONADO
+  const modal = document.getElementById('modal');
+
+  const modalDialog = document.createElement('div');
+  modalDialog.classList.add('modal-dialog');
+
+  const modalContent = document.createElement('div')
+  modalContent.classList.add('modal-content');
+
+  //nome
+  const nome = document.createElement('h5');
+  nome.classList.add('modal-title');
+  nome.innerHTML = pokemon.name;
+
+  //imagem
+  const foto = document.createElement('img');
+  foto.src = pokemon.sprites.front_default;  
+
+  //tipo
+  const tipo = document.createElement('div');
+  tipo.classList.add('modal-body');
+  tipo.innerHTML = pokemon.types;
+
+  //peso
+  const peso = document.createElement('div');
+  peso.classList.add('modal-body');
+  peso.innerHTML = pokemon.weight;
+
+  //altura
+  const altura = document.createElement('div');
+  altura.classList.add('modal-body');
+  altura.innerHTML = pokemon.height;
+
+  //id
+  const id = document.createElement('div');
+  id.classList.add('modal-body');
+  id.innerHTML = pokemon.id;
+
+  modalContent.appendChild(modalDialog);
+  modalContent.appendChild(nome);
+  modalContent.appendChild(foto);
+  modalContent.appendChild(tipo);
+  modalContent.appendChild(peso);
+  modalContent.appendChild(altura);
+  modalContent.appendChild(id);
+  modal.appendChild(modalContent);
 }
